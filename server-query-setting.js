695899d64bb8311301a07b015b73921ac94397a6
PAUTHIER Jonas <jonas.pauthier@gmail.com>
2019-02-27 22:31:54 +0100

    Add server for query setting example

---
+++ server-query-setting.js
@@ -0,0 +1,38 @@
+const Hapi = require('hapi')
+const Joi = require('joi')
+const Qs = require('qs')
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost',
+  query: {
+    parser: query => Qs.parse(query),
+  },
+})
+
+server.route({
+  method: 'GET',
+  path: '/server-query-setting',
+  handler(request, h) {
+    return request.query
+  },
+  options: {
+    validate: {
+      query: {
+        status: Joi.array(),
+      },
+    },
+  },
+})
+
+const init = async () => {
+  await server.start()
+  console.log(`Server running at: ${server.info.uri}`)
+}
+
+process.on('unhandledRejection', err => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
