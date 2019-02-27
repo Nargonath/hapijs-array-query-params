76181363774785615d5d8cfb0fe0e9014af433f4
PAUTHIER Jonas <jonas.pauthier@gmail.com>
2019-02-27 22:31:28 +0100

    Add server for Joi example

---
+++ server-joi-example.js
@@ -0,0 +1,36 @@
+const Hapi = require('hapi')
+const Joi = require('joi')
+
+const server = Hapi.server({
+  port: 3100,
+  host: 'localhost',
+})
+
+server.route({
+  method: 'GET',
+  path: '/server-joi-example',
+  handler(request, h) {
+    return request.query
+  },
+  options: {
+    validate: {
+      query: Joi.object({
+        status: Joi.array()
+          .single()
+          .items(Joi.string()),
+      }).rename('status[]', 'status', { ignoreUndefined: true }),
+    },
+  },
+})
+
+const init = async () => {
+  await server.start()
+  console.log(`Server running at: ${server.info.uri}`)
+}
+
+process.on('unhandledRejection', err => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
